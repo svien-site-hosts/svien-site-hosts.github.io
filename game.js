9a7801c148dc8a4bf0266cc09d8dc2484c15f47b
bubs-fricked-up-site <lime.thepup@gmail.com>
2026-06-23 21:45:24 +0800

    a

--- game.js
+++ game.js
@@ -2,11 +2,15 @@ var cv = document.getElementById("cv");
 var c  = cv.getContext('2d');
 
 var bpm = 136;
-var beat = bpm / 60;
+var beat = 60 / bpm;
 var loopDur = beat * 4 * 8;
-var soundCharacters = [
-	new Audio("sound/b1.wav"), // 0
-	new Audio("sound/b1.wav"), // 1
+var soundCharactersLoopA = [
+	new Audio("sound/b1a.wav"), // 0
+	new Audio("sound/b2.wav"), // 1
+];
+var soundCharactersLoopB = [
+	new Audio("sound/b1b.wav"), // 0
+	new Audio("sound/b2.wav"), // 1
 ];
 
 var dt = 1;
